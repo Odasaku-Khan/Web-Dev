fbd2d9adab942caf7de2ab12063855ade25c5a98
Odasaku-Khan <tenseizangetsu3@gmail.com>
2025-04-23 20:08:03 +0500

    hate Angular

--- project/front_job_search/src/app/auth/login/login.component.ts
+++ project/front_job_search/src/app/auth/login/login.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule,FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm:FormGroup;
+  error:string='';
 
+  constructor(
+    private fb:FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ){
+    this.loginForm=this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required],
+    });
+  }
+
+  OnSubmit(){
+    if (this.loginForm.valid) {
+      const{username,password}=this.loginForm.value;
+      this.authService.login(username,password).subscribe({
+        next:(res)=>{
+          localStorage.setItem('access',res.access);
+          localStorage.setItem('refresh',res.refresh);
+          this.router.navigate(['/dashboard'])
+        },
+        error:(err)=>{
+          this.error='inavalid errors'
+        }
+      })
+    }
+  }
 }
--- project/front_job_search/src/app/services/auth.service.ts
+++ project/front_job_search/src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+interface Token{
+  access:string;
+  refresh:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl='http://127.0.0.1:8000'
+  constructor(private http: HttpClient) { }
+  login(username:string,password:string):Observable<Token>{
+    return this.http.post<Token>(`${this.baseUrl}/tokrn/`,{
+    username,
+    password,
+    })
+  }
+  logout():void{
+    localStorage.removeItem('access'),
+    localStorage.removeItem('refresh')
+  }
 
-  constructor() { }
+  isAuthenticated(): boolean{
+    return !localStorage.getItem('access')
+  }
 }
